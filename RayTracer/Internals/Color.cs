e0a1834125d06d75915a9f4e6a0a12775c22d298
Fabio Cryns <crynsfabio@hotmail.com>
2017-09-29 17:36:26 +0200

    Advanced c# tot aan Async

---
+++ RayTracer/Internals/Color.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayTracer.Internals {
+    public class Color {
+        public double R;
+        public double G;
+        public double B;
+
+        public Color(double r, double g, double b) { R = r; G = g; B = b; }
+        public Color(string str) {
+            string[] nums = str.Split(',');
+            if (nums.Length != 3) throw new ArgumentException();
+            CultureInfo formatProvider = new CultureInfo("en-US");
+            R = double.Parse(nums[0], formatProvider);
+            G = double.Parse(nums[1], formatProvider);
+            B = double.Parse(nums[2], formatProvider);
+        }
+
+        public static Color Make(double r, double g, double b) { return new Color(r, g, b); }
+
+        public static Color Times(double n, Color v) {
+            return new Color(n * v.R, n * v.G, n * v.B);
+        }
+        public static Color Times(Color v1, Color v2) {
+            return new Color(v1.R * v2.R, v1.G * v2.G, v1.B * v2.B);
+        }
+
+        public static Color Plus(Color v1, Color v2) {
+            return new Color(v1.R + v2.R, v1.G + v2.G, v1.B + v2.B);
+        }
+        public static Color Minus(Color v1, Color v2) {
+            return new Color(v1.R - v2.R, v1.G - v2.G, v1.B - v2.B);
+        }
+
+        public static readonly Color Background = Make(0, 0, 0);
+        public static readonly Color DefaultColor = Make(0, 0, 0);
+
+        private static double Legalize(double d) {
+            return d > 1 ? 1 : d;
+        }
+
+        // Explicit conversion from Color to System.Drawin.Color
+        public static explicit operator System.Drawing.Color (Color color)
+        {
+            return System.Drawing.Color.FromArgb((int)(Legalize(color.R) * 255), (int)(Legalize(color.G) * 255), (int)(Legalize(color.B) * 255));
+        }
+        // Implicit conversion from double to Digit
+        public static implicit operator Color (System.Drawing.Color color)
+        {
+            return new Color(color.R, color.G, color.B);
+        }
+    }
+}
---
+++ RayTracer/Internals/Surfaces.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayTracer.Internals {
+    public static class Surfaces {
+        public static Surface FindSurface(string name) {
+            Type surfaceAttribute = Type.GetType("RayTracer.Internals.SurfaceAttribute");
+            Type surfaceType = Type.GetType("RayTracer.Internals.Surfaces");
+            foreach (var mi in surfaceType.GetFields())
+            {
+                foreach (var a in mi.CustomAttributes)
+                {
+                    if (a.AttributeType == surfaceAttribute)
+                    {
+                        // we have a member of the class that is marked as a Surface
+                        if (mi.Name == name)
+                        {
+                            return mi.GetValue(null) as Surface;
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+
+        // Only works with X-Z plane.
+        [Surface]
+        public static readonly Surface CheckerBoard =
+            new Surface() {
+                Diffuse = pos => ((Math.Floor(pos.Z) + Math.Floor(pos.X)) % 2 != 0)
+                                    ? Color.Make(1, 1, 1)
+                                    : Color.Make(0, 0, 0),
+                Specular = pos => Color.Make(1, 1, 1),
+                Reflect = pos => ((Math.Floor(pos.Z) + Math.Floor(pos.X)) % 2 != 0)
+                                    ? .1
+                                    : .7,
+                Roughness = 150
+            };
+
+        [Surface]
+        public static readonly Surface Shiny =
+            new Surface() {
+                Diffuse = pos => Color.Make(1, 1, 1),
+                Specular = pos => Color.Make(.5, .5, .5),
+                Reflect = pos => .6,
+                Roughness = 50
+            };
+
+        [Surface]
+        public static readonly Surface NonShiny =
+            new Surface() {
+                Diffuse = pos => Color.Make(1, 1, 1),
+                Specular = pos => Color.Make(.5, .5, .5),
+                Reflect = pos => 0,
+                Roughness = 50
+            };
+    }
+
+    [AttributeUsage(AttributeTargets.Field)]
+    public class SurfaceAttribute : Attribute
+    {
+        //
+    }
+}
